1a36dfd312138fbdf125935cac1b6b8cbbfb55ee
immanuelpena <49746752+immanuelpena@users.noreply.github.com>
2021-10-23 21:53:27 -0300

    Commit

--- Charts generator.py
+++ Charts generator.py
@@ -4,6 +4,7 @@ import os
 import matplotlib
 import matplotlib.pyplot as plt
 import time
+import random
 from faker import Faker
 
 fake = Faker()
@@ -11,7 +12,7 @@ fake = Faker()
 
 start_time = time.time()
 
-# for x in range(1,2):
+# for x in range(1,2):P
 #     plt.plot([0, 1, 2, 3, 4], [0, 3, 5, 9, 11])
 #     plt.xlabel('Months')
 #     plt.ylabel('Books Read')
@@ -19,23 +20,50 @@ start_time = time.time()
 
 # print("--- %s seconds ---" % (time.time() - start_time))
 
+#fake.add_provider(credit_card)
 
 # function to create a dataframe with fake values for our workers
 def make_chart_data(num):
     
-    # lists to randomly assign to workers
-    status_list = ['Full Time', 'Part Time', 'Per Diem']
-    team_list = [fake.color_name() for x in range(4)]
+    dimensions = ['Name', 'Date','CreditCard','Company','WorkerStatus','Position','Country']
+    measures = ['Salary', 'Quantity','Sales','Discount']
+
+    status_list = ['Active','Inactive','Suspended']
+    team_list = [fake.job() for x in range(4)]
+
+    fake_data = [{'Name':fake.name(),
+                  'Date':fake.date_between(start_date='-5y', end_date='+1y'), 
+                  'CreditCard':fake.credit_card_provider(), 
+                  'Salary':random.randrange(1, 5000, 2), 
+                  'Quantity':random.randrange(1, 200, 2),
+                  'Sales':random.randrange(50000, 1000000, 2),
+                  'Discount':random.random(),
+                  'Company':fake.company(),
+                  'WorkerStatus':np.random.choice(status_list, p=[0.50, 0.40, 0.10]), # assign items from list with different probabilities
+                  'Position':np.random.choice(team_list),
+                  'Country':fake.country()} for x in range(num)
+                  ]
+
+    return dimensions, measures, fake_data
+
+
+dimensions, measures, fake_data = make_chart_data(num=15)
+
+
+
+def generate_chart(dimensions, measures, fake_data):
     
+    fake_data = pd.DataFrame(fake_data)    
+  
+    x_axis_value = random.choice(dimensions)
+    y_axis_value = random.choice(measures)
+    
+    x_values = fake_data[x_axis_value].unique()
+    y_values = fake_data[x_axis_value].value_counts().tolist()
 
-    fake_workers = [{'Worker ID':x+1000,
-                  'Worker Name':fake.name(), 
-                  'Hire Date':fake.date_between(start_date='-30y', end_date='today'),
-                  'Worker Status':np.random.choice(status_list, p=[0.50, 0.30, 0.20]), # assign items from list with different probabilities
-                  'Team':np.random.choice(team_list)} for x in range(num)]
-        
-    return fake_workers
+    plt.bar(x_values, y_values)
+    plt.show()
 
-worker_df = pd.DataFrame(make_chart_data(num=15))
+    
+fig = generate_chart(dimensions, measures, fake_data)
 
-print('asd1')
